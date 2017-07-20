2b54670046323e0db79c44083f73b50b47d088d7
ashik.khan <ashik.khan@N1020.spftl.com>
2017-07-20 17:36:06 +0600

    Model n View

---
+++ src/main/java/com/controller/student/StudentController.java
@@ -0,0 +1,11 @@
+package com.controller.student;
+
+import org.springframework.web.servlet.ModelAndView;
+
+public class StudentController {
+	
+	public ModelAndView initializeStudent()
+	{
+		return new ModelAndView();
+	}
+}
